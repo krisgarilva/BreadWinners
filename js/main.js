25bd015a36f03228b0771d68fd2e1106ef48be53
iamcindy <22-76323@g.batstate-u.edu.ph>
2025-06-25 15:37:01 +0800

    Added Function in toggleMenu on Main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,12 @@ document.getElementById("title").innerText = "Welcome";
 document.getElementById("menuBtn").addEventListener("click", toggleMenu);
 
 // toggleMenu is not defined
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  // Toggle the visibility of the menu
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
